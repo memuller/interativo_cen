e150df5bf4daaf37052111b21096c5e91856d2c7
Matheus E. Muller <hello@memuller.com>
2012-03-22 21:01:31 -0300

    Updates gem requirements.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,7 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "pauldix-feedzirra", :lib => "feedzirra", :source => "http://gems.github.com"
+  config.gem "feedzirra"
 	config.gem "hash_extension", :source => "http://gemcutter.org"
 	config.gem 'hoptoad_notifier'
 	config.gem "haml"
